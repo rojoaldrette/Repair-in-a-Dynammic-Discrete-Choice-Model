8413224e0715e83f3a14678d56cb2943a3127969
Rodrigo Aldrette <roryaldrettes@hotmail.com>
2026-06-30 22:39:48 +0900

    Commit jsut to check

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -13,12 +13,19 @@
 #
 # _____________________________________________________________________________
 
+import jax
 import jax.numpy as jnp
 from jaxopt import FixedPointIteration
+from jaxopt import AndersonAcceleration
 
 # Procedure --------------------
-# 1. Define the utility matrix for each state
-# 
+# 1. Define the utility for each state
+# 2. Define the dynamics to get a transition matrix
+# 3. Do the value functions with dynammics
+# 4. Get fixed point
+# 5. Solve for excess demand and get price vector
+
+
 
 
 
